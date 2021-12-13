f1e4cd2aad861ddc977e82af6461a5691d23959a
jpbarto <jason.p.barto@gmail.com>
2021-12-13 23:46:51 +0000

    cleaned up shell script

--- associate-managed-rule-group.sh
+++ associate-managed-rule-group.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-RSRC_ARN=""
 REGION="eu-west-1"
 API_ID="g8jk2lo2w3"
 STAGE_NAME="v2"
+RSRC_ARN="arn:aws:apigateway:${REGION}::/restapis/${API_ID}/stages/${STAGE_NAME}"
 
-ARN=$(aws wafv2 create-web-acl --cli-input-json file:///tmp/create-wacl-slim.json --query 'Summary.ARN' --region ${REGION})
-aws wafv2 associate-web-acl --resource-arn arn:aws:apigateway:${REGION}::/restapis/${API_ID}/stages/${STAGE_NAME} --web-acl-arn ${ARN} --region ${REGION}
+ARN=$(aws wafv2 create-web-acl --cli-input-json file://create-wacl-slim.json --query 'Summary.ARN' --region ${REGION})
+aws wafv2 associate-web-acl --resource-arn ${RSRC_ARN} --web-acl-arn ${ARN} --region ${REGION}
